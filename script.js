2e9f60d182fc3037484da51090c33f6ed7f8c31e
pratyaksha-verma06 <vermapratyaksha51@gmail.com>
2026-04-19 17:39:22 +0530

    Added the download pdf feature and reset feature

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function linktopreview(inputId,previewId){
 
     input.addEventListener("input",()=>{
        if(input.tagName==="TEXTAREA"){
-        preview.innerHTML=input.value.replace("/\n\g","<br>")||" ";
+        preview.innerHTML = input.value.replace(/\n/g, "<br>") || " ";
        }
         else{
         preview.innerText=input.value||" ";
@@ -22,3 +22,43 @@ linktopreview("skills","p-skills");
 linktopreview("projects","p-projects");
 
 
+// Reset Button 
+const reset=document.querySelector("#resetbtn");
+
+function resetPreview(){
+    const reset_clmn={
+         "p-name": "Your Name",
+        "p-email": "Email",
+        "p-contact": "Phone",
+        "p-education": "",
+        "p-skills": "",
+        "p-projects": ""
+    };
+    for (let id in reset_clmn) {
+    document.getElementById(id).innerText = reset_clmn[id];
+    }
+};
+
+reset.addEventListener("click",resetPreview);
+
+
+// download PDF
+async function downloadPDF() {
+    const { jsPDF } = window.jspdf;
+    const element = document.querySelector("#resume-content");
+
+    const canvas = await html2canvas(element, {
+        scale: 2 
+    });
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p", "mm", "a4");
+    const imgWidth = 210; 
+    const pageHeight = 295;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+    pdf.save("resume.pdf");
+}
+
+const downloadBtn = document.getElementById("downloadBtn");
+downloadBtn.addEventListener("click", downloadPDF);
